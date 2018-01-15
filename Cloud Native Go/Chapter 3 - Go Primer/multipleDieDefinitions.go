d1748f39492bf80734a9c2a89ffe372a0180795f
Todai88 <joabaj88@gmail.com>
2018-01-15 22:28:38 +0000

    finished some more work on chapter 3

---
+++ Cloud Native Go/Chapter 3 - Go Primer/multipleDieDefinitions.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type dieRollFunc func(int) int
+
+func dieRoll(size int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(size) + 1
+}
+
+func fakeDieRoll(size int) int {
+	return 42
+}
+
+func getDieRolls() []dieRollFunc {
+	return []dieRollFunc{
+		dieRoll,
+		fakeDieRoll,
+	}
+}
+
+func main() {
+	var rolls = getDieRolls()
+
+	for index, rollFunc := range rolls {
+		fmt.Printf("Die Roll Attempt #%d, result: %d\n", index, rollFunc(10))
+	}
+}
